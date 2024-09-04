0877ac51d32fd8c8a68dd5306878669effbf6ad9
Andreas Rozek <A.Rozek@gmx.de>
2024-09-04 07:31:03 +0200

    new build

--- dist/syncableStore.esm.js
+++ dist/syncableStore.esm.js
@@ -7,34 +7,27 @@ export { Box, SyncedArray, SyncedDoc, SyncedMap, SyncedText, SyncedXml, Y, areSa
 *                                                                              *
 *******************************************************************************/
 /**** syncableStore ****/
-var StoreOfRoot = new WeakMap();
-function syncableStore(Callback, reportClosestArrayObject, sharedDoc) {
-    if (reportClosestArrayObject === void 0) { reportClosestArrayObject = false; }
-    var sharedStore = syncedStore({ Root: {} }, sharedDoc);
-    var StoreRoot = sharedStore.Root;
+const StoreOfRoot = new WeakMap();
+function syncableStore(Callback, reportClosestArrayObject = false, sharedDoc) {
+    const sharedStore = syncedStore({ Root: {} }, sharedDoc);
+    const StoreRoot = sharedStore.Root;
     StoreOfRoot.set(StoreRoot, sharedStore);
     StoreEntryOfYjsItem.set(getYjsValue(StoreRoot), StoreRoot);
     if (Callback != null) {
-        observeDeep(StoreRoot, function () {
-            var ArgList = [];
-            for (var _i = 0; _i < arguments.length; _i++) {
-                ArgList[_i] = arguments[_i];
-            }
-            return reportChanges(ArgList[1], StoreRoot, reportClosestArrayObject, Callback);
-        });
+        observeDeep(StoreRoot, (...ArgList) => reportChanges(ArgList[1], StoreRoot, reportClosestArrayObject, Callback));
     }
     return sharedStore.Root;
 }
 /**** getYjsDoc (modified for syncableStore) ****/
 function getYjsDoc(StoreRoot) {
-    var Store = StoreOfRoot.get(StoreRoot);
+    const Store = StoreOfRoot.get(StoreRoot);
     if (Store == null)
         throw new Error('NoSuchStore: the given object is not a valid "SyncableStore"');
     return getYjsDoc$1(Store);
 }
 /**** transact (modified for syncableStore) ****/
 function transact(StoreRoot, Callback) {
-    var YjsDoc = getYjsDoc(StoreRoot);
+    const YjsDoc = getYjsDoc(StoreRoot);
     if (YjsDoc == null)
         throw new Error('NoYjsDoc: no Yjs document for the given "SyncableStore" found');
     // @ts-ignore TS2345 don't be too strict regarding the function signature
@@ -42,9 +35,9 @@ function transact(StoreRoot, Callback) {
 }
 /**** reportChanges ****/
 function reportChanges(YjsTransaction, StoreRoot, reportClosestArrayObject, Callback) {
-    var Transaction = new Map();
-    YjsTransaction.changed.forEach(function (PropertySet, Container) {
-        var changedEntry = StoreEntryForYjsItem(Container, StoreRoot);
+    const Transaction = new Map();
+    YjsTransaction.changed.forEach((PropertySet, Container) => {
+        let changedEntry = StoreEntryForYjsItem(Container, StoreRoot);
         if (changedEntry != null) {
             if (Array.isArray(changedEntry) && reportClosestArrayObject) {
                 reportClosestObjectWithArray(Container, StoreRoot, Transaction);
@@ -55,16 +48,15 @@ function reportChanges(YjsTransaction, StoreRoot, reportClosestArrayObject, Call
                     Transaction.set(changedEntry, []);
                 }
                 else {
-                    for (var _i = 0, PropertySet_1 = PropertySet; _i < PropertySet_1.length; _i++) {
-                        var Property = PropertySet_1[_i];
-                        var Entry = changedEntry[Property];
-                        var YjsItem = getYjsValue(Entry);
+                    for (let Property of PropertySet) {
+                        const Entry = changedEntry[Property];
+                        const YjsItem = getYjsValue(Entry);
                         if (YjsItem != null) {
                             StoreEntryOfYjsItem.set(YjsItem, Entry);
                             memoizeContentsOf(Entry);
                         }
                     }
-                    Transaction.set(changedEntry, Array.from(PropertySet.values()).filter(function (Candidate) { return Candidate != null; }));
+                    Transaction.set(changedEntry, Array.from(PropertySet.values()).filter((Candidate) => Candidate != null));
                 }
             }
         }
@@ -72,33 +64,32 @@ function reportChanges(YjsTransaction, StoreRoot, reportClosestArrayObject, Call
     Callback(Transaction);
 }
 /**** StoreEntryForYjsItem ****/
-var StoreEntryOfYjsItem = new WeakMap();
+const StoreEntryOfYjsItem = new WeakMap();
 function StoreEntryForYjsItem(YjsItem, StoreRoot) {
-    var _a, _b, _c;
-    var knownEntry = StoreEntryOfYjsItem.get(YjsItem);
+    const knownEntry = StoreEntryOfYjsItem.get(YjsItem);
     if (knownEntry != null) {
         return knownEntry;
     }
-    var outerYjsItem = (((_a = YjsItem._item) === null || _a === void 0 ? void 0 : _a.parent) || ((_b = YjsItem._start) === null || _b === void 0 ? void 0 : _b.parent) || getYjsValue(StoreRoot));
+    const outerYjsItem = (YjsItem._item?.parent || YjsItem._start?.parent || getYjsValue(StoreRoot));
     if (outerYjsItem == null)
         throw new Error('NoRegisteredRoot: the given store root has no Yjs counterpart');
-    var outerEntry = StoreEntryOfYjsItem.get(outerYjsItem);
+    const outerEntry = StoreEntryOfYjsItem.get(outerYjsItem);
     if (outerEntry == null) {
         return undefined;
     }
-    var matchingEntry = undefined;
+    let matchingEntry = undefined;
     if (Array.isArray(outerEntry)) {
         matchingEntry = outerEntry.find(// scan array for changed element
-        function (Candidate) { return getYjsValue(Candidate) === YjsItem; });
+        (Candidate) => getYjsValue(Candidate) === YjsItem);
     }
     else {
-        var knownProperty = (_c = YjsItem._item) === null || _c === void 0 ? void 0 : _c.parentSub;
+        const knownProperty = YjsItem._item?.parentSub;
         if (typeof knownProperty === 'string') { // directly access changed item
             matchingEntry = outerEntry[knownProperty];
         }
         else {
             matchingEntry = outerEntry.values().find(// scan object for changed item
-            function (Candidate) { return getYjsValue(Candidate) === YjsItem; });
+            (Candidate) => getYjsValue(Candidate) === YjsItem);
         }
     }
     if (matchingEntry != null) { // memoize changed entry
@@ -109,13 +100,12 @@ function StoreEntryForYjsItem(YjsItem, StoreRoot) {
 }
 /**** reportClosestObjectWithArray ****/
 function reportClosestObjectWithArray(YjsItem, StoreRoot, Transaction) {
-    var _a, _b;
-    var outerYjsItem = (((_a = YjsItem._item) === null || _a === void 0 ? void 0 : _a.parent) || ((_b = YjsItem._start) === null || _b === void 0 ? void 0 : _b.parent) || getYjsValue(StoreRoot));
+    const outerYjsItem = (YjsItem._item?.parent || YjsItem._start?.parent || getYjsValue(StoreRoot));
     if (outerYjsItem._start == null) {
-        var outerEntry_1 = StoreEntryOfYjsItem.get(outerYjsItem);
-        var Property = Object.keys(outerEntry_1).find(// scan for prop. with YjsItem
-        function (Candidate) { return getYjsValue(outerEntry_1[Candidate]) === YjsItem; });
-        Transaction.set(outerEntry_1, [Property]);
+        let outerEntry = StoreEntryOfYjsItem.get(outerYjsItem);
+        let Property = Object.keys(outerEntry).find(// scan for prop. with YjsItem
+        (Candidate) => getYjsValue(outerEntry[Candidate]) === YjsItem);
+        Transaction.set(outerEntry, [Property]);
     }
     else {
         reportClosestObjectWithArray(outerYjsItem, StoreRoot, Transaction);
@@ -123,9 +113,9 @@ function reportClosestObjectWithArray(YjsItem, StoreRoot, Transaction) {
 }
 /**** reportChanges ****/
 function memoizeContentsOf(StoreEntry) {
-    var EntryList = (Array.isArray(StoreEntry) ? StoreEntry : Object.values(StoreEntry));
-    EntryList.forEach(function (Entry) {
-        var YjsItem = getYjsValue(Entry);
+    let EntryList = (Array.isArray(StoreEntry) ? StoreEntry : Object.values(StoreEntry));
+    EntryList.forEach((Entry) => {
+        const YjsItem = getYjsValue(Entry);
         if (YjsItem != null) {
             StoreEntryOfYjsItem.set(YjsItem, Entry);
             memoizeContentsOf(Entry);
