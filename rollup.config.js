add43c00bd841396c591c715d414079536d04a12
Andreas Rozek <A.Rozek@gmx.de>
2024-09-29 06:57:17 +0200

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,13 @@
 // see https://github.com/rozek/build-configuration-study
 
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser'
+import terser from '@rollup/plugin-terser'
 
 export default {
   input: './src/syncableStore.ts',
   output: [
     {
-      file:     './dist/syncableStore.js',
+      file:     './dist/syncableStore.umd.js',
       format:    'umd', // builds for both Node.js and Browser
       name:      'syncableStore', // required for UMD modules
       noConflict:true,
